426d7b7b375f14b1d2bb050b226018e6a8cc13f0
Nicolas Couture-Grenier <nicolas.couture.grenier@gmail.com>
2025-02-20 05:34:09 -0500

    ...

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>  
-	<v-app style="pa-4">
+	<v-app>
 		<v-main>
-		  <v-responsive class="align-center mx-auto" max-width="900">
+		  <v-responsive class="align-center mx-auto pa-4" max-width="900">
 			<h1>Super Sequencer 3k</h1>
 			<v-row>
         <v-col cols="6">
