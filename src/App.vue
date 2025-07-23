68093242dd6e4f3d1d5caf42515cce5da19d6cd3
Nicolas Couture-Grenier <nicolas.couture.grenier@gmail.com>
2025-07-23 16:53:17 -0400

    ....

--- src/App.vue
+++ src/App.vue
@@ -66,6 +66,7 @@
           />
         </v-col>
       </v-row>
+      <!--
       <v-row>
         <v-col cols="3">
           <v-switch 
@@ -92,6 +93,7 @@
           />
         </v-col>
       </v-row>
+      -->
 			<button @click="toggleSequencer" class="stopplay">{{ isRunning ? '⏹️' : '▶️' }}</button>
       <button @click="copyURL" class="userbutton">📋Copy URL</button>
 			<button @click="downloadMIDI" class="downloadmidi">Download MIDI</button>
